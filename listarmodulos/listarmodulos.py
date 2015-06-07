8ae3fcf2011ee28801886153f66ebf9bfcc4d1fc
Ruben <rubencabrera@eviland.co.uk>
2015-06-07 20:46:44 +0200

    Lista de módulos de un servidor

--- listarmodulos/listarmodulos.py
+++ listarmodulos/listarmodulos.py
@@ -32,9 +32,7 @@
 #            post: 
 # https://www.odoo.com/forum/help-1/question/how-to-uninstall-a-module-from-command-line-36076
 ###############################################################################
-"""Uninstall a module."""
-
-
+"""List installed modules."""
 
 
 import xmlrpclib
@@ -42,7 +40,9 @@ import argparse
 import getpass
 
 
-parser = argparse.ArgumentParser()
+
+
+parser = argparse.ArgumentParser(description='Listar módulos instalados')
 # Connection args
 # Argumentos de conexión
 parser.add_argument('-d', '--database', help="Use DATABASE as the database name",
@@ -53,42 +53,58 @@ parser.add_argument('-w', '--password',
                     help="Use PASSWORD as the database password.",
                     action='store', metavar='PASSWORD', default='admin')
 parser.add_argument('-s', '--url',
-                    help="Point to the web services hosted at URL",
-                    action='store', metavar='URL',
-                    default='http://localhost:8069')
+                    help="Point to the web services hosted at URL. Ojo: indicar el http:// ",
+                    action='store', metavar='URL')
 # Feature args
 # Argumentos del módulo
-parser.add_argument('module', help="Uninstall the module MODULE",
-                    action='store', metavar='MODULE')
+#parser.add_argument('module', help="Uninstall the module MODULE",
+#                    action='store', metavar='MODULE')
 
 args = vars(parser.parse_args())
+#login(args['url'],args['database'],args['user'],args['password'])
+
+
 
 # Log in
 ws_common = xmlrpclib.ServerProxy(args['url'] + '/xmlrpc/common')
 uid = ws_common.login(args['database'], args['user'], args['password'])
 print "Logged in to the common web service."
+
 # Get the object proxy
 ws_object = xmlrpclib.ServerProxy(args['url'] + '/xmlrpc/object')
 print "Connected to the object web service."
+#listarModulos()
 
 
 
-
-# Find the parent location by name
-# Encontrar la ubicación del módulo a partir del nombre
+# Encontrar la lista de módulos instalados por id
+# Esta búsqueda deja fuera otors estados, como el 
+# "pendiente de actualizar" que es 'to upgrade'
 res_ids = ws_object.execute(
     args['database'], uid, args['password'],
-    'ir.module.module', 'search', [('name', '=', args['module'])])
-if len(res_ids) != 1:
+    'ir.module.module', 'search', [('state', '=', 'installed')])
+if len(res_ids) < 1:
     raise Exception("Search failed")
 
-# Uninstall the module
-# Desisntalación
-print "Uninstalling '%s'" % args['module']
-result = ws_object.execute(
-    args['database'], uid, args['password'],
-    'ir.module.module', "button_immediate_uninstall", res_ids)
-print "All done."
 
+# Con la lista de ids, sacar el nombre de los módulos.
+lista_modulos = ws_object.execute(args['database'],uid,args['password'],
+    'ir.module.module', 'read', res_ids, ['name']
+    )
+# La web que viene en "website" es inútil a la hora de 
+# descargar, es una web de referencia del módulo. 
+
+# lista_modulos es una lista de diccionarios con la pareja
+# {id, name}, con lo siguiente nos quedamos sólo con los nombres.
+
+lista_limpia = list()
+
+for element in lista_modulos:
+    lista_limpia.append(element['name'])
+
+
+# Aquí iría bien un menú o una forma de imprimir esto guay:
+
+print lista_limpia
 
 
